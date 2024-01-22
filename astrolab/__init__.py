2222b135e2bc8c9eb5823647c7eedc8d3c402c77
Philip Cherian <dpcherian@gmail.com>
2024-01-22 16:46:46 +0530

    Fixed `__init__.py` to return `__version__` and `__author__` information.

--- astrolab/__init__.py
+++ astrolab/__init__.py
@@ -1,7 +1,14 @@
+# Use the ``importlib.metadata`` package (known as ``importlib_metadata`` in Python < 3.8) to access the metadata of the package and return its version and author.
+
 try:
-    from importlib.metadata import metadata as __metadata__
-    __version__ = __metadata__(__name__)['version']
-    __author__  = __metadata__(__name__)['Author-email']
-except:
-    __version__ = "Not yet installed!"
+    import importlib.metadata as importlib_metadata # For Python >= 3.8
+except ImportError:
+    import importlib_metadata as importlib_metadata # For Python < 3.8
+
+try:
+    # Use the in-built __package__ or __name__ variables to get metadata. Using __package__ allows for the case where __name__ is "__main__"
+    __version__ = importlib_metadata.metadata(__package__ or __name__)['version']
+    __author__  = importlib_metadata.metadata(__package__ or __name__)['Author-email']
+except importlib_metadata.PackageNotFoundError:
+    __version__ = "Not installed as package yet!"
     __author__  = "Philip Cherian"
