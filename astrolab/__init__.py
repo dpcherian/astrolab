ba1362c626b2723b3637c9df4dce5fd7ec6eaad6
Philip Cherian <dpcherian@gmail.com>
2024-01-22 13:09:31 +0530

    Re-introduced `importlib.metadata`.
    Build issue in ReadTheDocs will hopefully be fixed by including `importlib` in documentation's `requirements.txt`.

--- astrolab/__init__.py
+++ astrolab/__init__.py
@@ -1,1 +1,7 @@
-
+try:
+    from importlib.metadata import metadata as __metadata__
+    __version__ = __metadata__(__name__)['version']
+    __author__  = __metadata__(__name__)['Author-email']
+except:
+    __version__ = "Not yet installed!"
+    __author__  = "Philip Cherian"
